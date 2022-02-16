4d2370d42f836ddb4516d3351a44f37dd73b2d7a
felipe <feliperestrepomeneses@gmail.com>
2022-02-15 23:00:45 -0500

    Empezamos creando nuestro model de Todo y agregamos ya algunos Todos a la DB

---
+++ todolist/todo/models.py
@@ -0,0 +1,20 @@
+import datetime
+from django.db import models
+
+from time import timezone
+
+# Create your models here. Or edit, but remember always makemigration afer that
+ 
+class Todo(models.Model):
+    description = models.CharField(max_length=200)
+    pub_date = models.DateTimeField("date published")
+
+    def __str__(self):
+        """This method returns the description when get the object"""
+        return self.description
+    
+    def was_published_recently(self):
+        """This method show us true or false if the Todo was recently published"""
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+    
+    
