547da351c3564e0ed67fa3703188b3316a38e170
felipe <feliperestrepomeneses@gmail.com>
2022-02-17 23:52:55 -0500

    agregamos el primer template

--- todolist/todo/urls.py
+++ todolist/todo/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("<int:todo_id>/", views.detail, name="index"),
+    path("<int:todo_id>/results", views.results, name="index"),
 ]
--- todolist/todo/views.py
+++ todolist/todo/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Todo
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello World")
+    latest_todo_list = Todo.objects.all()
+    return render(request, "todo/index.html", {
+        "latest_todo_list": latest_todo_list
+    })
+
+def detail(request, todo_id):
+    return HttpResponse(f'Estas viendo el To-Do numero {todo_id}')
+
+def results(request, todo_id):
+    return HttpResponse(f'El to-do {todo_id} ya fue completado?')
