04e876b0775e67c9b05931069560fad546bea2e3
felipe <feliperestrepomeneses@gmail.com>
2022-02-21 23:51:48 -0500

    agregamos el task add en metodo post, pero aun no funciona

--- todolist/todo/views.py
+++ todolist/todo/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
 from .models import Todo
 
@@ -6,8 +6,15 @@ from .models import Todo
 
 def index(request):
     latest_todo_list = Todo.objects.all()
+    if request.method == "POST":    
+        if "taskAdd" in request.POST:
+            content = request.POST["description"]
+            content = Todo
+            content.save()
+            # Me pide un positional argument self para poder agregar el nuevo task
+            return redirect("/")
     return render(request, "todo/index.html", {
-        "latest_todo_list": latest_todo_list
+        "latest_todo_list": latest_todo_list,
     })
 
 def detail(request, todo_id):
